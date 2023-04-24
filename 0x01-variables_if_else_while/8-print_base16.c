de5ccd317cac24f3fb635a72a733af25a637e456
Mimifresh <mimigolld@gmail.com>
2023-04-24 10:52:35 -0700

    print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - all the numbers of base 16 in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	char lc;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+
+	for (lc = 'a'; lc <= 'f'; lc++)
+	{
+		putchar(lc);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
