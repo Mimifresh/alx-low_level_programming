663eb20d0d751852f6d482a606a58de1698f68e9
Mimifresh <mimigolld@gmail.com>
2023-04-26 14:39:57 -0700

    abs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#inclue "main.h"
+#include "main.h"
 
 /**
  *  print_sign - prints the sign of a number
@@ -14,13 +14,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
-	}
-		_putchar(48);
+	if else (n == 0)
+	{
+		_putchar('0');
 		return (0);
-	} else if (n < 0)
+	{
+	if else (n < 0)
 	{
 		_putchar('-');
-	}
 		return (-1);
+	}
 }
