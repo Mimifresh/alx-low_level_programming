b2b645a32d7cc6f63090becfad9732131544880c
Mimifresh <mimigolld@gmail.com>
2023-05-10 09:35:33 -0700

    strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string to be printed
+ * base case: when s is 0
+ * Return: on success 1
+ */
+int _strlen_recursion(char *s)
+}
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	}
+	return (1 + _strlen_recursion(s + 1));
+	}
+}
