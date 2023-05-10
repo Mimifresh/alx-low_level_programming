064d7ad0e59fff932c319c3376d19a8a24bc5bbb
Mimifresh <mimigolld@gmail.com>
2023-05-10 06:22:16 -0700

    print rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: points to a sttring as imput.
+ *
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\n')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
