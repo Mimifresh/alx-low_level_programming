7621c13ab0348f6ef6f6c6446dda991e7fe33297
Mimifresh <mimigolld@gmail.com>
2023-05-17 10:01:43 -0700

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,162 +1,36 @@
-
 #include "main.h"
-
-                
-
 #include <stdlib.h>
-
-                
-
 /**
-
-                
-
- * alloc_grid - nested loop to make grid
-
-                
-
- * @width: width input
-
-                
-
- * @height: height input
-
-                
-
- * Return: pointer to 2 dim. array
-
-                
-
- */
-
-                
-
+* alloc_grid - nested loop to make grid
+* @width: width input
+* @height: height input
+* Return: pointer to 2 dim. array
+*/
 int **alloc_grid(int width, int height)
-
-                
-
 {
-
-                
-
-        int **mee;
-
-                
-
-        int x, y;
-
-                
-
-
-                
-
-        if (width <= 0 || height <= 0)
-
-                
-
-                return (NULL);
-
-                
-
-
-                
-
-        mee = malloc(sizeof(int *) * height);
-
-                
-
-
-                
-
-        if (mee == NULL)
-
-                
-
-                return (NULL);
-
-                
-
-
-                
-
-        for (x = 0; x < height; x++)
-
-                
-
-        {
-
-                
-
-                mee[x] = malloc(sizeof(int) * width);
-
-                
-
-
-                
-
-                if (mee[x] == NULL)
-
-                
-
-                {
-
-                
-
-                        for (; x >= 0; x--)
-
-                
-
-                                free(mee[x]);
-
-                
-
-
-                
-
-                        free(mee);
-
-                
-
-                        return (NULL);
-
-                
-
-                }
-
-                
-
-        }
-
-                
-
-
-                
-
-        for (x = 0; x < height; x++)
-
-                
-
-        {
-
-                
-
-                for (y = 0; y < width; y++)
-
-                
-
-                        mee[x][y] = 0;
-
-                
-
-        }
-
-                
-
-
-                
-
-        return (mee);
-
-                
-
+	int **mi;
+	int a, b;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	mi = malloc(sizeof(int *) * height);
+	if (mi == NULL)
+		return (NULL);
+	for (a = 0; a < height; a++)
+	{
+		mi[a] = malloc(sizeof(int) * width);
+		if (mi[a] == NULL)
+		{
+			for (a == 0; a >= 0; a--)
+				free(mi[a]);
+		free(mi);
+		return (NULL);
+		}
+	}
+	for (a = 0; a < height; a++)
+	{
+		for (b = 0; b < width; b++)
+			mi[a][b] = 0;
+	}
+	return (mi);
 }
