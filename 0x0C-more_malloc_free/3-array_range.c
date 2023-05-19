8ef878bbdc9aa7969787f613baceeaea984867fa
Mimifresh <mimigolld@gmail.com>
2023-05-19 04:27:34 -0700

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -5,7 +5,7 @@
 * @min: minimum range of values stored
 * @max: maximum range of values stored and number of elements
 * Return: pointer to the new array
-*/		
+*/
 int *array_range(int min, int max)
 {
 	int *pt;
