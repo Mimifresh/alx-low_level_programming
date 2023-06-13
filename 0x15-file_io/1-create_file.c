ecd3c466cd143a01ba47753ef94bbc6959415a42
Mimifresh <mimigolld@gmail.com>
2023-06-13 12:02:29 -0700

    created a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+* create_file - creates a file.
+* @filename: name of file.
+* @text_content: content of the txt.
+* Return: -1 on fail and 1 on success.
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int numletter;
+	int ctr;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
+	for (numletter = 0; text_content[numletter]; numletter++)
+		;
+
+	ctr = write(fd, text_content, numletter);
+
+	if (ctr == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
